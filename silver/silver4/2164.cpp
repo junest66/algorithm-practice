fc6ed95929bddd9561c0d0706e62f5a247e90584
junest66 <chlwndks333@gmail.com>
2023-01-31 18:41:56 +0900

    Add 2164.cpp

---
+++ silver/silver4/2164.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int n;
+    queue<int> que;
+    cin >> n;
+    for(int i=1; i<=n; i++) que.push(i);
+    while(que.size()!=1) {
+        que.pop();
+        que.push(que.front());
+        que.pop();
+    }
+    cout << que.front();
+}
