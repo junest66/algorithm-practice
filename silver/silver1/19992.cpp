9b13e01d354ec2ad591194c7b8386a20fbb9aa92
junest66 <chlwndks333@gmail.com>
2023-04-29 18:51:21 +0900

    Retrying 19992.cpp

--- silver/silver1/19992.cpp
+++ silver/silver1/19992.cpp
@@ -1,48 +1,43 @@
+// 
+
 #include <iostream>
 #include <string>
 using namespace std;
-int board[64][64];
 
-bool check(int n, int y, int x) {
-    int temp = board[y][x];
-    for(int i=y; i<y+n; i++) {
-        for(int j=x; j<x+n; j++) {
-            if(temp!=board[i][j]) return false;
+int n;
+string board[65];
+string ans;
+
+void func(int a, int b, int n) {
+    if(n==1) { //base case
+        ans+=board[a][b];
+        return;
+    }
+    char start = board[a][b];
+    for(int i=a; i<a+n; i++) {
+        for(int j=b; j<b+n; j++) {
+            if(start!=board[i][j]) {
+                ans.append("(");
+                func(a,b,n/2);
+                func(a,b+n/2,n/2);
+                func(a+n/2,b,n/2);
+                func(a+n/2,b+n/2,n/2);
+                ans.append(")");
+                return;
+            }
         }
     }
-    cout << temp;
-    return true;
-}
-
-void func(int n, int y, int x) {
-   if(n==1) {
-      cout<< board[y][x];
-      return;
-   }
-   if(check(n,y,x)) return;
-   else {
-        int half = n /2 ;
-        cout << "(";
-        func(half,y,x);
-        func(half,y, x+half);
-        func(half,y+half,x);
-        func(half,y+half, x+half);
-        cout << ")";
-   }
-
+    ans+=board[a][b];
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
+
     cin >> n;
     for(int i=0; i<n; i++) {
-        string str;
-        cin >> str;
-        for(int j =0; j<n; j++) {
-            board[i][j] = str[j] - '0';
-        }
+        cin >> board[i];
     }
-    func(n, 0, 0);
+    func(0,0,n);
+    cout << ans;
 }
