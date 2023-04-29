918ca34013819e18d2ef1d111f0f2f30e54be237
junest66 <chlwndks333@gmail.com>
2023-04-29 20:08:56 +0900

    Retrying 2447.cpp

--- gold/gold5/2447.cpp
+++ gold/gold5/2447.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 using namespace std;
-int n;
 
-void func(int n, int a, int b) {
+int n;
+char board[6562][6562];
+
+void func(int a, int b, int n) {
     if(n==1) {
-        cout << "*";
+        board[a][b] = '*';
+        return;
     }
-    else if (a / (n / 3) % 3 == 1 && b / (n / 3) % 3 == 1) {
-        cout << " ";
+
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            if(i==1 && j==1) {
+                continue;
+            }
+            func(a+i*n/3,b+j*n/3,n/3);
+        }
     }
-    else func(n/3,a,b);
+    return;
 }
+
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    fill(&board[0][0], &board[0][0] + 6562 * 6562, ' ');
     cin >> n;
+    func(0,0,n);
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            func(n,i,j);
+            cout << board[i][j];
         }
         cout << "\n";
     }
-    
 }
