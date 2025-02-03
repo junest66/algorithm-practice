8b10d7c626b7a0f5ffc6f8703314fc3667a829b7
junest66 <chlwndks333@gmail.com>
2025-02-03 17:07:59 +0900

    add: 11286 - 절대값 힙

---
+++ baekjoon-java/src/silver/silver1/Problem11286.java
@@ -0,0 +1,38 @@
+package silver.silver1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Problem11286 {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if (first_abs == second_abs) {
+                return o1 - o2;
+            }
+            return first_abs - second_abs;
+        });
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if (a == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.offer(a);
+            }
+        }
+    }
+}
