71542655f8839f2583194c3ea95c08139335b849
junest66 <chlwndks333@gmail.com>
2025-02-04 00:39:20 +0900

    add: 11726 - 2×n 타일링

---
+++ baekjoon-java/src/silver/silver3/Problem11726.java
@@ -0,0 +1,20 @@
+package silver.silver3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Problem11726 {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
