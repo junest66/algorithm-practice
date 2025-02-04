efb7a6508827d12e872086b4e2c4113d888599be
junest66 <chlwndks333@gmail.com>
2025-02-05 00:31:31 +0900

    refactor: 1717 - 집합의 표현

--- baekjoon-java/src/gold/gold5/Problem1717.java
+++ baekjoon-java/src/gold/gold5/Problem1717.java
@@ -1,78 +1,55 @@
 package gold.gold5;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Problem1717 {
-    public static class Union_Find {
-        int[] parent;
-        int[] rank;
+    static int[] parent;
 
-        public Union_Find(int size) {
-            this.parent = new int[size+1];
-            this.rank = new int[size+1];
-            init();
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) { // init
+            parent[i] = i;
         }
 
-        public void init() {
-            for (int i = 1; i < parent.length; i++) {
-                parent[i] = i;
-                rank[i] = 1;
-            }
-        }
-
-        public int find(int x) {
-            if(x != parent[x]) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
-
-        public void union(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-            if (px != py) {
-                if (rank[px] > rank[py]) {
-                    parent[py] = px;
-                } else if (rank[px] < rank[py]) {
-                    parent[px] = py;
+        for (int i = 0; i < m; i++) {
+            int question = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (question == 0) { // union
+                union(a, b);
+            } else { // same
+                if (checkSum(a, b)) {
+                    System.out.println("YES");
                 } else {
-                    parent[px] = py;
-                    rank[py]++;
+                    System.out.println("NO");
                 }
             }
         }
 
-        public void is_connected(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-            if (px != py) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
+    }
+
+    private static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) {
+            parent[pb] = pa;
         }
     }
-    public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        Union_Find unionFind = new Union_Find(n);
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if (a == 0) {
-                unionFind.union(b, c);
-            } else {
-                unionFind.is_connected(b, c);
-            }
+
+    private static int find(int n) { //경로 압축
+        if (n != parent[n]) {
+            parent[n] = find(parent[n]);
         }
+        return parent[n];
+    }
+
+    private static boolean checkSum(int a, int b) {
+        return find(a) == find(b);
     }
 }
