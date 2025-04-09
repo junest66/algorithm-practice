0ea0e78aadeba94df3b00fa3fa81eb6e546e01c9
Jun Choi <chlwndks333@gmail.com>
2025-04-09 14:29:59 +0900

    Title: 이상한 진수 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250409/이상한 진수 2/awkward-digits-2.java
+++ 250409/이상한 진수 2/awkward-digits-2.java
@@ -3,14 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        StringBuilder sb = new StringBuilder(a);
+        int answer = Integer.MIN_VALUE;
         for(int i = 1; i < a.length(); i++) {
-            if(a.charAt(i) == '0') {
-                sb.setCharAt(i, '1');
-                break;
-            }
+            StringBuilder sb = new StringBuilder(a);
+            sb.setCharAt(i, func(a.charAt(i)));
+            answer = Math.max(Integer.parseInt(sb.toString(), 2), answer);
         }
-        System.out.println(Integer.parseInt(sb.toString(), 2));
         // Please write your code here.
+        System.out.println(answer);
+    }
+    
+    private static char func(char a) {
+        return a == '0' ? '1' : '0';
     }
 }
