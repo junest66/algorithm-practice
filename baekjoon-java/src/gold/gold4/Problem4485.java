04f9e679d45da01ddd2ed9d82d0adfd6ae475430
junest66 <chlwndks333@gmail.com>
2025-05-19 01:03:43 +0900

    refactor: gold4 - 녹색 옷 입은 애가 젤다지?
    - 다시 품

--- baekjoon-java/src/gold/gold4/Problem4485.java
+++ baekjoon-java/src/gold/gold4/Problem4485.java
@@ -5,79 +5,60 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Problem4485 {
-    // 시작부터 목적지까지 최소한의 가중치의 크기를 구하는 문제
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
+    static int[][] d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 0;
+        int index = 1;
+        int[][] arr;
+        int[][] dp;
         while (true) {
-            count++;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(br.readLine());
             if (n == 0) {
                 break;
             }
-            int[][] arr = new int[n][n];
+            arr = new int[n][n];
+            dp = new int[n][n];
+            StringTokenizer st;
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
+                Arrays.fill(dp[i], Integer.MAX_VALUE);
             }
-            int minDist = dijkstra(arr, n);
-            System.out.println("Problem " + count + ": " + minDist);
-        }
-    }
-
-    private static class Node implements Comparable<Node> {
-        int dist;
-        int x;
-        int y;
-
-        public Node(int dist, int y, int x) {
-            this.dist = dist;
-            this.y = y;
-            this.x = x;
-        }
-
-        @Override
-        public int compareTo(Node other) {
-            return Integer.compare(dist, other.dist);
-        }
-    }
-
-    private static int dijkstra(int[][] arr, int n) {
-        int[][] d = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(d[i], Integer.MAX_VALUE);
-        }
-        PriorityQueue<Node> pq = new PriorityQueue<Node>();
-        pq.offer(new Node(arr[0][0], 0, 0));
-        d[0][0] = arr[0][0];
-        while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            if (d[current.y][current.x] != current.dist) {
-                continue;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+            dp[0][0] = arr[0][0];
+            PriorityQueue<int[]> pq = new PriorityQueue<>(
+                    Comparator.comparing(a -> a[2])); // 가중치 오름차순 0 -> y, 1 -> x, 2 -> dist
+            pq.offer(new int[]{0, 0, dp[0][0]});
+            while (!pq.isEmpty()) {
+                int[] current = pq.poll();
+                int cy = current[0];
+                int cx = current[1];
+                int cd = current[2];
+                if (dp[cy][cx] < cd) {
                     continue;
                 }
-                if (d[ny][nx] > d[current.y][current.x] + arr[ny][nx]) {
-                    d[ny][nx] = d[current.y][current.x] + arr[ny][nx];
-                    pq.offer(new Node(d[ny][nx], ny, nx));
+                for (int[] x : d) {
+                    int ny = x[0] + cy;
+                    int nx = x[1] + cx;
+                    if (ny < 0 || nx < 0 || ny >= n || nx >= n) {
+                        continue;
+                    }
+                    int nextCost = dp[cy][cx] + arr[ny][nx];
+                    if (nextCost < dp[ny][nx]) {
+                        dp[ny][nx] = nextCost;
+                        pq.offer(new int[]{ny, nx, nextCost});
+                    }
                 }
             }
+            System.out.println("Problem " + index++ + ": " + dp[n - 1][n - 1]);
         }
-        return d[n - 1][n - 1];
     }
 }
