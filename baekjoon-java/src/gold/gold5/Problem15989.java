5e0a18b8a9244ebddfd8ba0961a428e91dc9574e
junest66 <chlwndks333@gmail.com>
2025-06-13 15:00:42 +0900

    retry: gold5 - 1, 2, 3 더하기 4

--- baekjoon-java/src/gold/gold5/Problem15989.java
+++ baekjoon-java/src/gold/gold5/Problem15989.java
@@ -4,40 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Problem15989 {
+    static int t;
+
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/input.txt"));
+        System.setIn(new FileInputStream("baekjoon-java/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(br.readLine());
+        int[] arr = {1, 2, 3};
+        int[] nArr = new int[t];
+        int max = 0;
         for (int i = 0; i < t; i++) {
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            solution(n);
+            nArr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(nArr[i], max);
         }
-
-    }
-
-    private static void solution(int n) {
-        //dp[i][j]  i를 만드는데 끝자리가 j면서 오름차순
-        //dp[i][1] = d[i-1][1]   i를 만드는데 끝자리가 1이면서 오름차순으로 i를 만드는 경우
-        //dp[i][2] = d[i-2][1] + d[i-2][2]   i를 만드는데 끝자리가 2이면서 오름차순으로 i를 만드는 경우
-        //dp[i][3] = d[i-2][1] + d[i-2][2] + d[i-3][3]   i를 만드는데 끝자리가 3이면서 오름차순으로 만드는 경우
-        int[][] dp = new int[10002][4];
-        dp[1][1] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 1;
-        dp[3][1] = 1;
-        dp[3][2] = 1;
-        dp[3][3] = 1;
-        for (int i = 4; i <= n; i++) {
-            dp[i][1] = dp[i - 1][1];
-            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
-            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i-3][3];
+        int[] dp = new int[max + 1];
+        dp[0] = 1;
+        for (int x : arr) {
+            for (int i = 1; i <= max; i++) {
+                if (i >= x) {
+                    dp[i] += dp[i - x];
+                }
+            }
         }
-        System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
-
+        for (int i : nArr) {
+            System.out.println(dp[i]);
+        }
     }
 }
